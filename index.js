effa42bb272e24ba8ad4dc85b6368803bc2ea0aa
KavetiRohith <rohitkaveti@gmail.com>
2023-03-24 14:09:44 +0530

    split out stuff

--- index.js
+++ index.js
@@ -28,54 +28,6 @@ client.on("auth_failure", (msg) => {
 
 client.on("ready", () => {
     console.log("Client is ready!");
-
-    const app = express();
-    const port = process.env.WHATSAPP_GATEWAY_PORT | 3676;
-
-    app.use(express.json());
-
-    app.post("/", async (req, res) => {
-        try {
-            const message = req.body.message;
-            const mobile_no = req.body.mobile_no;
-
-            if (message == null || mobile_no == null) {
-                return res.status(400).json({ "description": "message and mobile_no must be non null" });
-            }
-
-            let mobile_no_string = mobile_no.toString();
-
-            if (mobile_no_string.startsWith("+91") && mobile_no_string.length == 13) {
-                mobile_no_string = mobile_no_string.substring(1) + "@c.us";
-            } else if (mobile_no_string.length == 10) {
-                mobile_no_string = "91" + mobile_no_string + "@c.us";
-            } else {
-                return res.status(400).json({ "description": "Invalid Mobile Number" });
-            }
-
-            const isRegistered = await client.isRegisteredUser(mobile_no_string);
-
-            if (isRegistered) {
-                const sendMessageData = await client.sendMessage(mobile_no_string, message);
-                if (sendMessageData.ack != MessageAck.ACK_ERROR) {
-                    return res.status(200).json({ "description": "OK" });
-                }
-                return res.status(400).json({ "description": `Received Error: ${sendMessageData.ack.toString()} Acknowledgement` });
-
-            } else {
-                return res.status(400).json({ "description": `No Whatsapp User with Mobile number ${mobile_no}` });
-            }
-        } catch (err) {
-            console.error(err)
-            return res.status(500).json({ "description": "Internal Server Error" })
-        }
-
-        return res.status(500).json({ "description": "Uh Oh!" })
-    });
-
-    app.listen(port, () => {
-        console.log(`app listening on port ${port}`);
-    });
 });
 
 client.on("disconnected", (reason) => {
@@ -89,4 +41,64 @@ process.on("SIGINT", async () => {
     console.log("Received SIGINT Shutting down...");
     await client.destroy();
     process.exit(0);
+});
+
+async function sendWhatsAppMessage(client, mobile_no_string, message) {
+    try {
+        const isRegistered = await client.isRegisteredUser(mobile_no_string);
+
+        if (isRegistered) {
+            const sendMessageData = await client.sendMessage(mobile_no_string, message);
+            if (sendMessageData.ack != MessageAck.ACK_ERROR) {
+                console.log(`Sent Message Whatsapp for Mobile: ${mobile_no_string} Message: ${message}`)
+            } else {
+                console.log(`Received Error: ${sendMessageData.ack.toString()} Acknowledgement while sending ${message} to ${mobile_no_string}`)
+            }
+
+        } else {
+            console.log(`No Whatsapp User with Mobile number ${mobile_no} unable to send Message: ${message}`)
+        }
+    } catch (err) {
+        console.log(`Unable to send Message: ${message} to Mobile number ${mobile_no}`)
+        console.error(err)
+    }
+}
+
+const app = express();
+const port = process.env.WHATSAPP_GATEWAY_PORT | 3676;
+
+app.use(express.json());
+
+app.post("/", async (req, res) => {
+    try {
+        const message = req.body.message;
+        const mobile_no = req.body.mobile_no;
+
+        if (message == null || mobile_no == null) {
+            return res.status(400).json({ "description": "message and mobile_no must be non null" });
+        }
+
+        let mobile_no_string = mobile_no.toString();
+
+        if (mobile_no_string.startsWith("+91") && mobile_no_string.length == 13) {
+            mobile_no_string = mobile_no_string.substring(1) + "@c.us";
+        } else if (mobile_no_string.length == 10) {
+            mobile_no_string = "91" + mobile_no_string + "@c.us";
+        } else {
+            return res.status(400).json({ "description": "Invalid Mobile Number" });
+        }
+
+        await sendWhatsAppMessage(client, mobile_no_string, message)
+
+        return res.status(202).json({ "description": "OK" })
+    } catch (err) {
+        console.error(err)
+        return res.status(500).json({ "description": "Internal Server Error" })
+    }
+
+    return res.status(500).json({ "description": "Uh Oh!" })
+});
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`);
 });
